81bc2bacdcb67a7df108a30e325e8b0a841b88b4
Zubair Ansari <officialzubairansari@gmail.com>
2024-07-04 12:10:41 +0500

    message bug fix

--- api.php
+++ api.php
@@ -4,11 +4,7 @@
 $code = isset($_GET['code']) ? $_GET['code'] : '';
 
 // If the code isn't in the correct format, CloudFlare will throw a 1020
-if (!preg_match("/^([a-f0-9]{8})-(([a-f0-9]{4})-){3}([a-f0-9]{12}) *$/i", $code)) {
-	http_response_code(403);
-	echo "Your personal token is incorrect or is missing the required permission.";
-	die;
-
+if ( ! preg_match("/^([a-f0-9]{8})-(([a-f0-9]{4})-){3}([a-f0-9]{12}) *$/i", $code)) {
     http_response_code(403);
     echo <<<EOD
 {
@@ -16,14 +12,14 @@ if (!preg_match("/^([a-f0-9]{8})-(([a-f0-9]{4})-){3}([a-f0-9]{12}) *$/i", $code)
 	"description": "Your personal token is incorrect or is missing the required permission."
 }
 EOD;
-die;
+    die;
 }
 
 header('Content-Type: application/json; charset=utf-8');
 
 // Handle valid codes
 if ($code == "829862b2-59ef-42c9-894c-665208ac2f41") {
-	echo <<<EOD
+    echo <<<EOD
 {
   "amount": "100.00",
   "sold_at": "2024-09-07T10:54:28+10:00",
@@ -43,12 +39,10 @@ if ($code == "829862b2-59ef-42c9-894c-665208ac2f41") {
   "purchase_count": 1
 }
 EOD;
-}
-
-// Handle invalid codes
+} // Handle invalid codes
 else {
-	http_response_code(404);
-	echo <<<EOD
+    http_response_code(404);
+    echo <<<EOD
 {
 	"error": 404,
 	"description": "Purchase code was not found (or) did not belong to one of your items."
